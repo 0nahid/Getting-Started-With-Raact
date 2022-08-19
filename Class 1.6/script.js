766724b65f5665002025b0088f971e2b0a9de3c4
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-08-19 14:52:37 +0600

    pass dynamic id on every elements

--- Class 1.6/script.js
+++ Class 1.6/script.js
@@ -1,18 +1,33 @@
 // select dom elements
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
+const counterEl = document.getElementById("counter-0");
 const CloneDivEl = document.getElementById("clone-div");
 const addCounterBtn = document.getElementById("add-counter");
 const removeCounterBtn = document.getElementById("remove-counter");
+const inputNumber = document.getElementById("input-number");
+
 let index = 1;
+let inputValue = 1;
+const incrementEl = document.getElementById(`increment-${index - 1}`);
+const decrementEl = document.getElementById(`decrement-${index - 1}`);
+
+
+inputNumber.addEventListener("change", (e) => {
+    inputValue = e.target.value;
+    console.log(inputValue);
+    if (inputValue < 0) {
+        alert("Please enter a positive number");
+    }
+});
+
 
 addCounterBtn.addEventListener("click", () => {
     CloneDivEl.innerHTML += `
     <div id="counter-div"
         class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow mt-2">
+        <h2 class="">Counter ${index}</h2>
         <div class="text-2xl font-semibold" id="counter-${index}">0</div>
-        <div class="flex space-x-3">
+        <div class="flex space-x-1">
+        <input class="placeholder-shown:border-gray-500 ring ring-2 rounded px-2" type="number" name="" id="input-number-${index}" placeholder="1">
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id="increment-${index}">
                 Increment
             </button>
@@ -27,8 +42,8 @@ addCounterBtn.addEventListener("click", () => {
 
 
 // actions identifiers
-const INCREMENT = `increment-${index}`;
-const DECREMENT = `decrement-${index}`;
+const INCREMENT = `increment-${index - 1}`;
+const DECREMENT = `decrement-${index - 1}`;
 
 // actions creators
 const increment = (payload) => ({
@@ -49,6 +64,7 @@ const initialState = {
 
 // reducer
 const counterReducer = (state = initialState, action) => {
+    console.log(action);
     if (action.type === INCREMENT) {
         return {
             ...state,
@@ -59,16 +75,6 @@ const counterReducer = (state = initialState, action) => {
             ...state,
             value: state.value - action.payload,
         };
-    }
-    else if (action.type === "INTEST") {
-        const updatedState = {
-            ...state,
-            properties: {
-                ...state.properties,
-                b: state.properties.b + 1
-            }
-        }
-        return updatedState;
     }
     else {
         return state;
@@ -91,8 +97,8 @@ store.subscribe(render);
 
 // button click handlers
 incrementEl.addEventListener("click", () => {
-    store.dispatch(increment(1));
+    store.dispatch(increment(parseInt(inputValue) || 1));
 });
 decrementEl.addEventListener("click", () => {
-    store.dispatch(decrement(1));
+    store.dispatch(decrement(parseInt(inputValue) || 1));
 });
